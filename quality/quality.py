f628bfa9749623a0b78e8b7e246085f2e5097726
Vitória Alves <65989342+alvesvitoria@users.noreply.github.com>
2023-09-06 22:28:36 -0300

    Add job quality

--- quality/quality.py
+++ quality/quality.py
@@ -1,32 +1,60 @@
-from pyspark.sql import functions as F
-from pyspark.sql import SparkSession, Row
-import great_expectations as ge
+import great_expectations as gx
+import pandas as pd
 from datetime import date
+from pyspark.sql import SparkSession
 
 date_now = date.today()
-spark = SparkSession.builder.appName('Quality').getOrCreate()
+
+spark = SparkSession.builder.appName('Ingest').getOrCreate()
+
+def process_ge():
+  
+  context = gx.get_context()
 
 
-def qualityDatasets():
+  datasource = context.sources.add_spark(name="spark_datasource")
 
-    df_quality = ge.read_parquet("gs://ingest-207c30567ab/target/"+str(date_now))
+  dataframe = spark.read.format("parquet").option("header", "true").load("gs://ingest-207c30567ab/prepared/banks/2023-08-31/part-00000-864c720b-cb1b-4ad4-b0fc-0a8ca22e11de-c000.snappy.parquet")
 
-    # Verificando se os valores existem na coluna segmento
-    df_quality.expect_column_values_to_be_in_set(
+  name = "my_df_asset"
+  data_asset = datasource.add_dataframe_asset(name=name)
+  my_batch_request = data_asset.build_batch_request(dataframe=dataframe)
+
+
+  context.add_or_update_expectation_suite("my_expectation_suite")
+
+  validator = context.get_validator(
+      batch_request=my_batch_request,
+      expectation_suite_name="my_expectation_suite",
+  )
+  validator.head()
+
+  return validator
+
+
+def add_tests_suite(validator):
+
+    validator.expect_column_values_to_not_be_null(column="cnpj")
+
+    validator.expect_column_values_to_be_in_set(
     "segmento",
-    ["S1", "S2", "S3"])
+    ["S1", "S2", "S3", "S4", "S5"])
 
-    # Verificando se o campo existe
-    df_quality.expect_column_to_exist('cnpj')
+    validator.expect_column_values_to_be_unique('cnpj')
 
-    # Verificando se existem valores null no campo cnpj
-    df_quality.expect_column_values_to_not_be_null('cnpj')
+    validator.validate()
+    
+    config = validator.save_expectation_suite('gs://ingest-207c30567ab/quality/'+str(date_now)+'quality.json')
 
-    # Verificando se o campo cnpj é único
-    df_quality.expect_column_values_to_be_unique('cnpj')
-
-    df_quality.validate()
-
-    df_quality.save_expectation_suite("gs://ingest-207c30567ab/target/"+str(date_now)+".json")
+    return validator
 
 
+def main ():
+   
+   dataset_validate = process_ge()
+
+   add_tests_suite(dataset_validate)
+
+  
+if __name__ == "__main__":
+  main()
